ca63cf01d23f6a8edffc79c0c86b625837672991
Jan Bauer <jan.bauer@kiwi.com>
2024-08-06 19:32:18 +0200

    add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import notFound from "$lib/images/404.gif";
+</script>
+
+<div class="h-full grid place-content-center justify-items-center">
+	<h1 class="text-4xl font-bold text-center mb-4">Oops, nothing to find here...</h1>
+	<img src={notFound} alt="404" />
+</div>
